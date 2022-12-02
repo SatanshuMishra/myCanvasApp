b9d65e4778750ca6d9fd221dbb91b104946fc5c0
SatanshuMishra <satanshumishra@outlook.com>
2022-12-01 19:34:24 -0800

    V0.1.4

--- app/src/main/java/com/example/mycanvasapp/StudentView.java
+++ app/src/main/java/com/example/mycanvasapp/StudentView.java
@@ -9,17 +9,38 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.mycanvasapp.databinding.ActivityStudentViewBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Locale;
 
 public class StudentView extends AppCompatActivity {
 
+    RequestQueue queue;
+    String url;
+    String id = "100281";
+    JsonObjectRequest JSONObjectRequest;
+    String text;
+    Boolean locked = true;
+    JSONObject data;
     private ActivityStudentViewBinding binding;
     TextView pageName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityStudentViewBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        FetchData();
+
         replaceFragament(new OverviewFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -60,10 +81,51 @@ public class StudentView extends AppCompatActivity {
         });
     }
 
+//    @Override
+//    protected void onResume()
+//    {
+//        super.onResume();
+//        getName();
+//    }
+
     private void replaceFragament(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
     }
+
+    void FetchData(){
+        this.queue = Volley.newRequestQueue(this);
+        this.url = "https://canvas.instructure.com/api/v1/users/self/profile?access_token=11224~GdddS9ac9XWbgPRVzgoVYsFAlIskfOSntSMgOgGSyJeagI7Rnf9uq4PluJo8Czil";
+        this.JSONObjectRequest = new JsonObjectRequest(Request.Method.GET,this.url,null,new Response.Listener<JSONObject>(){
+            @Override
+            public void onResponse(JSONObject response) {
+                TextView headerName = findViewById(R.id.custom_header_name);
+                try {
+                    headerName.setHorizontallyScrolling(false);
+                    String name = "HELLO\n" + response.getString("name").split(" ")[0].toUpperCase();
+                    headerName.setText(name);
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Errr", Toast.LENGTH_SHORT);
+            }
+        });
+        queue.add(this.JSONObjectRequest);
+    }
+
+    public void getName(){
+        try{
+            System.out.println("QUERY: " + data.getString("name"));
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
 }
